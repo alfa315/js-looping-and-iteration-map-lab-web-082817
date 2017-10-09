9925e65681ffe5ceb91139a9ef8e837271214127
github username <github email address>
2017-10-09 14:22:04 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+  // const drivers = ['Bobby Smith', 'Sammy Watkins', 'Sally Jenkins', 'Annette Sawyer', 'Sarah Hucklebee', 'bobby ridge'];
+
 // Code your solution in this file.
+function lowerCaseDrivers(drivers) {
+  return drivers.map(function(driver) {
+    return driver.toLowerCase()
+  })
+}
+
+function nameToAttributes(drivers) {
+  return drivers.map(function(driver) {
+    return {
+      firstName: driver.split(" ")[0],
+      lastName: driver.split(" ")[1]
+    }
+  })
+}
+
+function attributesToPhrase(drivers) {
+  return drivers.map(function(driver) {
+    return `${driver.name} is from ${driver.hometown}`
+  })
+}
